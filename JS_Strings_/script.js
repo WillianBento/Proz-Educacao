ede8760c707c1fd3f60263d30cdf7af65af54c43
WillianBento <105550902+WillianBento@users.noreply.github.com>
2023-10-27 21:55:38 -0300

    Strings ${} novo metodo

---
+++ JS_Strings_/script.js
@@ -0,0 +1,10 @@
+let operacoesBasicas(num1, num2) {
+    console.log(`${num1} + ${num2} = ${num1 + num2}`);
+    console.log(`${num1} - ${num2} = ${num1 - num2}`);
+    console.log(`${num1} x ${num2} = ${num1 * num2}`);
+    console.log(`${num1} / ${num2} = ${num1 / num2}`);
+  }
+  
+  // Exemplo de uso da função
+  operacoesBasicas(4, 5);
+  
